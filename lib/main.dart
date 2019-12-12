82f34f9581e31029a07bc0e33d0122d0a78677bf
conqsolid <conqsolid@gmail.com>
2019-12-12 22:06:00 +0300

    column and container tries

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MiCardApp());
+}
+
+class MiCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blueGrey,
+                child: Text("Hello"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text("Hello"),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.orangeAccent,
+                child: Text("Hello"),
+              ),
+              Container(
+                width: double.infinity,
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
